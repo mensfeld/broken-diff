dda8b8d34b4c5161da7fa8db00128ff839ae6011
Maciej Mensfeld <maciej@mensfeld.pl>
2020-07-24 18:28:03 +0200

    rem

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
 yarn install
-yarn run diff2html -F working.html -- HEAD~3
-
-git diff -M HEAD~3 --no-color > diff
-
-yarn run diff2html -F 'not-working.html' -i file diff
+yarn run diff2html -- HEAD~5
